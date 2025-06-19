08cf3fe2dfa47f601cf12fa788e8245ba512c8c7
Melissa <lissak230@gmail.com>
2025-06-18 22:30:52 -0300

    added menu class and show menu func

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package src;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al conversor de monedas.");
+    }
+}
---
+++ src/conversor/modelos/Menu.java
@@ -0,0 +1,20 @@
+package src.conversor.modelos;
+
+public class Menu {
+    public static void mostrarMenu() {
+        System.out.println("""
+                Bienvenido/a al Conversor de Moneda.
+                
+                Elija una opción válida:
+                
+                1) Dólar -> Peso argentino
+                2) Peso argentino -> Dólar
+                3) Dólar -> Real brasileño
+                4) Real brasileño -> Dólar
+                5) Dólar -> Peso colombiano
+                6) Peso colombiano -> Dólar
+                7) Salir
+                
+                """);
+    }
+}
